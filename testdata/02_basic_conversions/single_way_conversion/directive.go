5946052798fcc645c2133672370e1237d8cf345a
godcong <jumbycc@163.com>
2025-12-12 19:03:07 +0800

    feat(docs): add oneway conversion directive example to abgen testdata

--- testdata/02_basic_conversions/single_way_conversion/directive.go
+++ testdata/02_basic_conversions/single_way_conversion/directive.go
@@ -4,3 +4,4 @@ package single_way_conversion
 //go:abgen:package:path=github.com/origadmin/abgen/testdata/02_basic_conversions/single_way_conversion/target,alias=target
 
 //go:abgen:convert="source=source.User,target=target.UserDTO,direction=oneway"
+//go:abgen:convert:direction=oneway
